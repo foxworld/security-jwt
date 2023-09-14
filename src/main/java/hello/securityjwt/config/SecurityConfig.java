cd20dd9e08f814a896bdc562135567640c8106ef
PeterHoon <PeterHoon@DESKTOP-MFK0T72>
2023-09-14 15:19:42 +0900

    로그인

--- src/main/java/hello/securityjwt/config/SecurityConfig.java
+++ src/main/java/hello/securityjwt/config/SecurityConfig.java
@@ -2,17 +2,14 @@ package hello.securityjwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import hello.securityjwt.filter.JwtFilter3;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
